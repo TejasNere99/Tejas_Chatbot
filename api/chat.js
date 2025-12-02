ac700685002734d55023006d75168759cb1a7185
Tejas Nere <tejasnere94@gmail.com>
2025-12-02 19:07:13 +0530

    Initial commit

---
+++ api/chat.js
@@ -0,0 +1,29 @@
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(405).json({ error: "Method not allowed" });
+  }
+
+  const { message, system } = req.body;
+
+  try {
+    const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+
+    const model = genAI.getGenerativeModel({
+      model: "gemini-2.5-flash",
+      systemInstruction: system
+    });
+
+    const result = await model.generateContent(message);
+
+    res.status(200).json({
+      reply: result.response.text()
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      reply: "Server sad aahe re biduuu 😭🔥"
+    });
+  }
+}
